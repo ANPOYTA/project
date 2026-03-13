3c8b04cf924b174b20e6ba95bb25f49f7dda1d82
anpoyta <annapoyta@gmail.com>
2026-03-14 01:30:52 +0300

    Фікс пайплайну

--- tests/dorm.spec.js
+++ tests/dorm.spec.js
@@ -1,27 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('Verifying student addition', async ({ page }) => {
-    // Заходимо на сайт
     await page.goto('http://localhost:5173');
 
-    // Готуємося до того, що з'явиться вікно введення імені
-    // Цей код спрацює АВТОМАТИЧНО, як тільки вискочить prompt
     page.on('dialog', async (dialog) => {
         console.log('Dialog has appeared:', dialog.message());
-        await dialog.accept('Oleksiy test'); // Вводимо ім'я
+        await dialog.accept('Oleksiy test'); 
     });
 
-    // Чекаємо, поки кнопка з'явиться на екрані (про всяк випадок)
     const button = page.locator('#addStudentBtn');
     await expect(button).toBeVisible();
 
-    // Натискаємо на кнопку
     await button.click();
 
-    // Даємо Playwright 2 секунди, щоб ми встигли побачити результат оком
     await page.waitForTimeout(2000);
 
-    // Перевіряємо, чи з'явився текст у message-area
     const message = page.locator('#message-area');
     await expect(message).toContainText(
         'Student Oleksiy test successfully added!'
